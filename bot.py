f12ff81dd862f414a61e984f35a1f057a29a78ac
bobbysq <bobbysq@gmail.com>
2016-11-09 08:29:25 -0600

    Fixed amlookup command

--- bot.py
+++ bot.py
@@ -13,19 +13,20 @@ for i in groups:
 oldMsg = ""
 
 def andymark_item(partnumber):
-    url = 'http://www.andymark.com/product-p/am-'+str(partnumber)+'.htm'
-    r = urllib.urlopen(url).read()
-    soup = BeautifulSoup(r)
-    price = soup.find_all("span", itemprop="price")
-    if soup.title.get_text()=="AndyMark Robot Parts Kits Mecanum Omni Wheels":
-        return(None) #404 checking
-    else:
-        name = re.sub(r'\([^)]*\)', '', soup.title.get_text())
-        #print(price[0].text)
-        money = float(price[0].text.encode('utf8','ignore').replace('$',''))
-        return([url, name, money])
-        #print(re.sub(r'\([^)]*\)', '', soup.title.get_text())) #kill the parenthesis
-        #print(float(price[0].get_text()))
+	url = 'http://www.andymark.com/product-p/am-'+str(partnumber)+'.htm'
+	r = urllib.request.urlopen(url).read()
+	soup = BeautifulSoup(r, "html.parser")
+	price = soup.find_all("span", itemprop="price")
+	if soup.title.get_text()=="AndyMark Robot Parts Kits Mecanum Omni Wheels":
+		return(None) #404 checking
+	else:
+		name = re.sub(r'\([^)]*\)', '', soup.title.get_text())
+		#print(price[0].text)
+		#money = price[0].text.encode('utf8','ignore')
+		money = price[0].get_text()
+		return([url, name, money])
+		#print(re.sub(r'\([^)]*\)', '', soup.title.get_text())) #kill the parenthesis
+		#print(float(price[0].get_text()))
 
 while True:
 	latestMsg = group.messages().newest
@@ -37,6 +38,10 @@ while True:
 			if latestMsg.text.split(" ")[0] == "amlookup":
 				productNo = latestMsg.text.split(" ")[1]
 				part = andymark_item(int(productNo))
-				bot.post("The item you looked up is a "+part[1]+". It costs $"+part[2])
+				if part:
+					print(part)
+					bot.post("The item you looked up is a "+part[1]+". It costs "+part[2]+".")
+				else:
+					bot.post("Item not found.")
 		oldMsg = latestMsg
 	time.sleep(2)
