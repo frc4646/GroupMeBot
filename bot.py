6f87a692ada6603a8c1d479d140a4530a7b14565
bobbysq <bobbysq@gmail.com>
2016-11-09 08:03:15 -0600

    Initial commit, some initial code written and a command to look up AndyMark parts was added

---
+++ bot.py
@@ -0,0 +1,42 @@
+import groupy
+from groupy import Bot
+import time
+from bs4 import BeautifulSoup
+import re
+import urllib
+
+bot = Bot.list().first
+groups = groupy.Group.list()
+for i in groups:
+	if i.name == "TrunkBot Test":
+		group=i
+oldMsg = ""
+
+def andymark_item(partnumber):
+    url = 'http://www.andymark.com/product-p/am-'+str(partnumber)+'.htm'
+    r = urllib.urlopen(url).read()
+    soup = BeautifulSoup(r)
+    price = soup.find_all("span", itemprop="price")
+    if soup.title.get_text()=="AndyMark Robot Parts Kits Mecanum Omni Wheels":
+        return(None) #404 checking
+    else:
+        name = re.sub(r'\([^)]*\)', '', soup.title.get_text())
+        #print(price[0].text)
+        money = float(price[0].text.encode('utf8','ignore').replace('$',''))
+        return([url, name, money])
+        #print(re.sub(r'\([^)]*\)', '', soup.title.get_text())) #kill the parenthesis
+        #print(float(price[0].get_text()))
+
+while True:
+	latestMsg = group.messages().newest
+	if latestMsg != oldMsg:
+		print(latestMsg)
+		if latestMsg.text == "Hi bot!":
+			bot.post("Hi, "+latestMsg.name)
+		else:
+			if latestMsg.text.split(" ")[0] == "amlookup":
+				productNo = latestMsg.text.split(" ")[1]
+				part = andymark_item(int(productNo))
+				bot.post("The item you looked up is a "+part[1]+". It costs $"+part[2])
+		oldMsg = latestMsg
+	time.sleep(2)
